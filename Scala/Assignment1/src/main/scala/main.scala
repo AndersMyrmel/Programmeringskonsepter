6c0dd57b46974599cf61086a21de36044405822a
Anders <andersmyrmel@gmail.com>
2022-08-22 16:46:39 +0200

    Changed datatype for filtered lambda list

--- Scala/Assignment1/src/main/scala/main.scala
+++ Scala/Assignment1/src/main/scala/main.scala
@@ -12,7 +12,7 @@
 
   //Make an anonymous (lambda) function that takes int as input and returns boolean if the number is divisible by 2. Use it to filter a list.
   val lambdaFilter = (arr : Array[Int]) => arr.filter((_ % 2 == 0))
-  var filtered = lambdaFilter(a)
+  val filtered = lambdaFilter(a)
 }
   
 
