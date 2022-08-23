437bc05d11154822fa90a4a7a3388c3d0bb06d02
Anders <andersmyrmel@gmail.com>
2022-08-23 20:04:52 +0200

    Made some additions to extra assignment

--- Scala/Extra/main.scala
+++ Scala/Extra/main.scala
@@ -8,7 +8,8 @@ import scala.io.Source
 @main def run() = {
     val fileName = "./Scala/Extra/Unequal.txt";
     //readFile(fileName);
-    getPuzzle(fileName);
+    val puzzle = getPuzzle(fileName);
+    puzzle.foreach { row => row foreach print; println }
 }
 
 // Read the file, print out the number of puzzles and their sizes
@@ -22,16 +23,50 @@ def readFile(fileName : String) = {
     }
 }
 
-def getPuzzle(fileName : String) = {
-    var size = 0
-    //var puzzle = Array()
+// Generate a 2D array of the puzzle
+def getPuzzle(fileName : String): Array[Array[Int]] = {
+    val size = getSize(fileName);
+    val puzzle = Array.ofDim[Int](size, size);
+    var row = 0;
+    var column = 0;
+
+    for (line <- Source.fromFile(fileName).getLines.drop(2)){
+         for (i <- line){
+            if (i.isDigit){
+                println((i, (row, column)));
+                puzzle(row)(column) = i.toInt - 48;
+                row += 1;
+                if (row >= size -1 && column >= size -1){
+                    return puzzle;
+                }
+                else if (row  >= size){
+                    row = 0;
+                    column += 1;
+                }
+            }
+            else if (i.toChar == '_') {
+                println((0,(row, column)));
+                puzzle(row)(column) = 0;
+                row += 1;
+                if (row >= size -1 && column >= size -1){
+                    return puzzle;
+                }
+                else if (row  >= size){
+                    row = 0;
+                    column += 1;
+                }
+            }
+         }
+    }
+    return puzzle;
+}
+
+// Get the width and height of a puzzle
+def getSize(fileName : String) : Int = {
     for (line <- Source.fromFile(fileName).getLines){
          if (line contains "size"){
-            size = line(line.length - 1).toInt - 48
-            println(size)
-            
+            return (line(line.length - 1).toInt - 48);
         }
     }
-    
-    
+    return -1;
 }
