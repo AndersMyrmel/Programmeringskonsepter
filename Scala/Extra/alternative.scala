38cd8e51017831844a675b8875c88f2576b79a21
Anders <andersmyrmel@gmail.com>
2022-09-13 17:23:13 +0200

    Implemented solvepuzzle function

--- Scala/Extra/alternative.scala
+++ Scala/Extra/alternative.scala
@@ -15,22 +15,41 @@ import scala.io.Source
     val constraints = getConstraints(fileName, size);
     printPuzzle(puzzle, size)
     println()
-    val legal = isLegal(puzzle,constraints,size,2,2,3)
-    println(legal)
+    val res = solvePuzzle(puzzle, constraints, size)
+    printPuzzle(puzzle, size)
+    println(res)
+    
 }
 
 // Solve puzzle
-def solvePuzzle(puzzle: Array[Int], constraints: Array[Int]) : Boolean = {
-    return false;
+def solvePuzzle(puzzle: Array[Int], constraints: Array[Int], size: Int) : Boolean = {
+    val firstEmptySquare = puzzle.indexOf(0) // Find the first empty square in puzzle
+    if (firstEmptySquare == -1) {
+        return true; // If no empty squares exist the puzzle is solved
+    }
+
+    // Convert 1D array index to row and column
+    val row = firstEmptySquare / size
+    val col = firstEmptySquare % size
+    
+    for (number <- 1 to size){
+        val legalMove = isLegal(puzzle, constraints, size, row, col, number)
+        if (legalMove) {
+            puzzle(firstEmptySquare) = number
+            if (solvePuzzle(puzzle, constraints, size)) {
+                return true;
+            }
+            else {
+                puzzle(firstEmptySquare) = 0 // Erase number and backtrack
+            }
+        }
+    }
+    return false; 
+    
 }
 
 // Check wheter it is legal to assign a particular number to a given box on the grid
 def isLegal(puzzle: Array[Int], constraints: Array[Int], size: Int, row: Int, col: Int, number: Int) : Boolean = {
-    // Potentially delete this
-    if (puzzle(row*size+col) != 0) {
-        return false;
-    }
-
     val sameRow = puzzle.slice(row*size, row*size+size)
     val sameCol = puzzle.slice(col, puzzle.size).zipWithIndex.collect{case (x,i) if (i) % size == 0 => x}
     
