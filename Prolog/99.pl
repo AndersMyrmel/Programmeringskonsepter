b13f44116065c4acb189f39d4217d25fe17fa567
Anders <andersmyrmel@gmail.com>
2022-11-21 18:31:08 +0100

    start 2

--- Prolog/99.pl
+++ Prolog/99.pl
@@ -1,3 +1,11 @@
+% Find the last element of a list.
+
 lastElement([Head]) :- write(Head).
 lastElement([_|Tail]):-
     lastElement(Tail).
+
+% Find the last but one element of a list.
+
+secondLast([Head]) :- write(Head).
+secondLast([X,_], X) :-
+    secondLast([_|T], X) :- secondLast(T, X).
