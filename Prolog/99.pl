9700b77804a0e0ef1f803ac26af68ac705f71a40
Anders <andersmyrmel@gmail.com>
2022-11-24 20:28:18 +0100

    add feature

--- Prolog/99.pl
+++ Prolog/99.pl
@@ -4,8 +4,32 @@ lastElement([Head]) :- write(Head).
 lastElement([_|Tail]):-
     lastElement(Tail).
 
-% Find the last but one element of a list.
+% Assume given a set of facts of the form father(name1,name2) (name1 is the father of name2).
+%   Define a predicate brother(X,Y) which holds iff X and Y are brothers.
+%   Define a predicate cousin(X,Y) which holds iff X and Y are cousins.
+%   Define a predicate grandson(X,Y) which holds iff X is a grandson of Y.
+%   Define a predicate descendent(X,Y) which holds iff X is a descendent of Y.
 
-secondLast([Head]) :- write(Head).
-secondLast([X,_], X) :-
-    secondLast([_|T], X) :- secondLast(T, X).
+father(name1,name3).
+father(name2,name4).
+father(name5, name1).
+brother(name1, name2).
+
+brother(X,Y) :- 
+    father(Z,X), 
+    father(Z,Y), 
+    not(X=Y).
+
+cousin(X,Y) :-
+    father(Z,X),
+    father(W,Y),
+    brother(Z,W).
+
+grandson(X,Y) :-
+    father(Y,Z),
+    father(Z,X).
+
+descendent(X,Y) :-
+    father(Y,X).
+descendent(X,Y) :-
+    grandson(X,Y).
--- Prolog/ReadFile.pl
+++ Prolog/ReadFile.pl
@@ -12,3 +12,30 @@ read_file(Stream,[X|L]) :-
     read_line_to_codes(Stream,Codes),
     atom_chars(X, Codes),
     read_file(Stream,L), !.
+
+alt :-
+    open('Unequal.txt', read, Stream),
+    get_char(Stream, Char1),
+    process_the_stream(Char1, Stream),
+    close(Stream).
+
+process_the_stream(end_of_file, _) :- !.
+
+process_the_stream(Char, Stream) :-
+    write(Char),
+    get_char(Stream, Char2),
+    process_the_stream(Char2, Stream).
+
+getNumOfPuzzles :-
+    open('Unequal.txt', read, File).
+    read_until_stop(File),
+    close(File).
+
+read_until_stop('Unequal.txt', [L|Lines]) :-
+    read_line_to_codes(File, Codes), Codes \= end_of_file,
+    atom_codes(L, Codes),
+    L \= size,
+    !, read_until_stop(File, Lines).
+read_until_stop(_, []).
+
+
